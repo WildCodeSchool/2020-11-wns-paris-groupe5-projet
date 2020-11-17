85e4d274cfba0d51086e06b8e1230091a1d19be1
Niass <cheroubawan@hotmail.fr>
2020-11-17 21:09:43 +0100

    add package and setup  for sending email notification

--- back/routes/students.js
+++ back/routes/students.js
@@ -7,6 +7,7 @@ console.log("students.getAllWilders", students.getAllStudents)
 
 router.get("/api/students", students.getAllStudents);
 router.post("/api/student/create", students.create);
+router.post("/api/student/sendEmail", students.sendEmail);
 router.delete("/api/student/delete/:id", students.delete);
 
 module.exports = router;
