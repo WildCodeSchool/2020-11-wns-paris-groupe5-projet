ebda1a861350ae4389b3aa2a8fb840a43a4f66dc
Niass <cheroubawan@hotmail.fr>
2020-11-18 20:32:58 +0100

    add cors and nodemon packages

--- back/index.js
+++ back/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 require('dotenv').config()
 require("./db/mongoose");
 const studentRouter = require("./routes/students");
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(studentRouter);
 
