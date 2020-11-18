c1f1ad0e0778b391b9b481844c442fdf2b2bacf6
Vincent Kouoï <vincentkouoi@gmail.com>
2020-11-18 10:57:54 +0100

    insert list student

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Connexion from "./Connexion";
 import Menu1 from "./Menu1";
+import Home from './Home';
+
 
 function App() {
   return (
     <div className="App">
         <Menu1 />
+        <Home />
         <Connexion />
     </div>
   );
