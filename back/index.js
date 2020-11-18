9148c0f6419d36f8f8cdc0406566464824d92e44
Niass <cheroubawan@hotmail.fr>
2020-11-18 20:36:17 +0100

    change port

--- back/index.js
+++ back/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const cors = require("cors");
 require('dotenv').config()
 require("./db/mongoose");
+
+let PORT = process.env.PORT || 5000;
 const studentRouter = require("./routes/students");
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(studentRouter);
 
-app.listen(3000, () => console.log("Server is running on port  3000"));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
