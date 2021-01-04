f8280ef13d0029eab385d82943018fd657bb7651
Niass <cheroubawan@hotmail.fr>
2021-01-04 20:27:04 +0100

    add missing file

--- back/db/mongoose.js
+++ back/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 console.log("process.env.MONGODB_URL", process.env.MONGODB_URL)
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect("mongodb://mongodb:27017/Runschool", {
     // "auth":process.env.DB_USER ? { "authSource": "admin" }: "",
     // "user": process.env.DB_USER,
     // "pass": process.env.DB_PASS,
