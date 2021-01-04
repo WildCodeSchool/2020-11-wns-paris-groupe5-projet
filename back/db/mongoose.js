93bff14236e121066cc930d967de7d94c44a91c7
Niass <cheroubawan@hotmail.fr>
2021-01-04 20:25:59 +0100

    dockerise without docker-compose

--- back/db/mongoose.js
+++ back/db/mongoose.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 console.log("process.env.MONGODB_URL", process.env.MONGODB_URL)
 mongoose.connect(process.env.MONGODB_URL, {
-    "auth":process.env.DB_USER ? { "authSource": "admin" }: "",
-    "user": process.env.DB_USER,
-    "pass": process.env.DB_PASS,
+    // "auth":process.env.DB_USER ? { "authSource": "admin" }: "",
+    // "user": process.env.DB_USER,
+    // "pass": process.env.DB_PASS,
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex:true,
