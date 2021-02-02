bc466a499e719dc14cd72a96eeb9b5caa3e1e527
Niass <cheroubawan@hotmail.fr>
2021-02-02 20:08:21 +0100

    remove console and set test env

--- back/src/middlewares/error-handler.ts
+++ back/src/middlewares/error-handler.ts
@@ -3,11 +3,9 @@ import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof CustomError) {
-    console.log("catch ?");
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
-  console.log("no catch");
 
   res.status(400).send({ errors: [{ message: "Something went wrong!" }] });
 };
